53e963edcbf539f098142c73b5d1656b84dc789d
m-murchie <mpmm@st-andrews.ac.uk>
2016-06-16 15:06:14 +0100

    Add files via upload

--- R/bootstrap function.R
+++ R/bootstrap function.R
@@ -1,86 +1,83 @@
-#                    --- BOOTSTRAP FUNCTION: VERSION 1 ---
+#                         --- BOOTSTRAP FUNCTION ---
 
-rm(list = ls())
 
-boot <- function(data, B = 999, trunc) {
+boot.dht <- function(tables, B = 999, trunc, hist = FALSE) {
 # INPUTS:    data    dataframe containing data to be analysed, WITH Sample.Label
 #                    column of numeric values 
 #            B       number of bootstrap replicates
-#            hist    produce histogram of abundance estimates
+#            hist    produce histogram of abundance estimates and encounter rates
 #            trunc   truncation distance, necessary for meta.data 
-# OUTPUTS:   tables comparing bootstrap and original sample estimates
-#            histogram of abundance estimates, if hist = TRUE
+# OUTPUTS:   summary table for bootstrap abundance estimates
+#            histogram of abundance estimates and encounter rates, if hist = TRUE
 # FUNCTIONS: ddf
-  if (!("Sample.Label" %in% names(data))) {
+  if (!("Sample.Label" %in% names(tables$data))) {
     stop("Data must contain Sample.Label column to execute bootstrap")
   }
   
   # bootstrap estimates of abundance
-  strip.count <- max(data$Sample.Label)   # number of transects 
-  N.hats <- NULL 
+  lines <- unique(tables$obs.table$Sample.Label)
+  lines.count <- length(lines)   # number of transects
+  e.rates <- NULL   # encounter rates  
+  N.hats <- NULL   # abundance estimates
   
   for (i in 1:B) {
-    strip.sample <- sample(1:strip.count, strip.count, replace=TRUE)
-    boot.sample <- data[data$Sample.Label==strip.sample[1],]
-    for (j in 2:strip.count) {
-      boot.sample <- rbind(boot.sample, data[data$Sample.Label==strip.sample[j],])
+    line.sample <- sample(lines, lines.count, replace=TRUE)
+    boot.data <- tables$data[tables$data$Sample.Label==line.sample[1],]
+    boot.sample <- tables$sample.table[tables$sample.table$Sample.Label==
+                                         line.sample[1],]
+    for (j in 2:lines.count) {
+      boot.data <- rbind(boot.data, tables$data[tables$data$Sample.Label
+                                                ==line.sample[j],])
+      boot.sample <- rbind(boot.sample, tables$sample.table
+                           [tables$sample.table$Sample.Label==line.sample[j],])
     }
-    ddf.model <- ddf(method="trial.fi",
-                     mrmodel=~glm(link="logit", formula=~distance),
-                     data=boot.sample, meta.data = list(width=trunc))
-    N.hats[i] <- summary(ddf.model)["Nhat"]$Nhat   # store abundance estimate
+    boot.ddf <- ddf(method = 'io.fi', mrmodel=~glm(link='logit', formula=~distance), 
+                    data = boot.data, meta.data=list(width=trunc))
+    boot.effort <- sum(boot.sample$Effort)
+    covered.area <- 2*boot.effort*trunc
+    region.area <- tables$region.table$Area
+    N.hats[i] <- boot.ddf$Nhat*region.area/covered.area
+    e.rates[i] <- summary(boot.ddf)$n/boot.effort
   }
   
+  
+  # original data
+  original.effort <- sum(tables$sample.table$Effort)
+  original.ddf <- ddf(method = 'io.fi', mrmodel=~glm(link='logit', 
+                                                     formula=~distance), 
+                      data = boot.data, meta.data=list(width=trunc))
+  dht.results <- dht(original.ddf, tables$region.table, tables$sample.table, 
+                     tables$obs.table)
+  N.hats[B+1] <- dht.results$individuals$N$Estimate[[1]]
+  e.rates[B+1] <- summary(original.ddf)$n/original.effort
+  
+  
   # summary characteristics
+  N.hats <- sort(N.hats)
   boot.mean <- mean(N.hats)
   boot.se <- sd(N.hats)   
-  boot.CV <- boot.se/boot.mean  
+  boot.cv <- boot.se/boot.mean  
   
-  sample.mean <- summary(ddf.model.orig)["Nhat"]$Nhat
-  sample.se <- summary(ddf.model.orig)["Nhat.se"]$Nhat.se
-  sample.CV <- sample.se/sample.mean
-
-  result.mean <- c(boot.mean, sample.mean)
-  result.se <- c(boot.se, sample.se)
-  result.CV <- c(boot.CV, sample.CV)
   
-  result <- data.frame(Estimate = result.mean,
-                       SE       = result.se,
-                       CV       = result.CV)
-  rownames(result) <- c("Bootstrap", "Original")
+  # histogram of bootstap abundance estimates and encounter rates
+  if (hist == TRUE) {
+    par(mfrow=c(2,2))
+    hist(N.hats)
+    hist(e.rates)
+    par(mfrow=c(1,1))
+  }
+  
+  
+  # results
+  result <- data.frame(Estimate = boot.mean,
+                       SE       = boot.se,
+                       CV       = boot.cv,
+                       lcl      = N.hats[(B+1)*0.025],
+                       ucl      = N.hats[(B+1)*0.975])
+  rownames(result) <- c("Bootstrap")
   
   return(result)
 }
 
-#---
-
-## GOLF TEE EXAMPLE
-library(mrds)
-data("book.tee.data")
-
-## tidy up tee data
-tee.data <- book.tee.data$book.tee.dataframe
-tee.data$sex <- as.factor(tee.data$sex)             ## sex and exposure are factor
-tee.data$exposure <- as.factor(tee.data$exposure)   ## variables
-tee.region <- book.tee.data$book.tee.region
-tee.samples <- book.tee.data$book.tee.samples
-tee.obs <- book.tee.data$book.tee.obs
-
-## add sample label column to tee data
-tee.obs <- tee.obs[order(tee.obs$object),]
-Sample.Label <- tee.obs$Sample.Label
-Sample.Label <- rep(Sample.Label, each=2)
-tee.data["Sample.Label"] <- Sample.Label
-
-boot(tee.data, hist=TRUE, trunc = 4)
 
 #---
-
-## CRABEATER SEAL EXAMPLE
-
-library(Distance)
-crabseal <- read.csv("crabbieMRDS.csv")
-crabseal$Sample.Label <- as.numeric(crabseal$Sample.Label)   # number transects
-
-boot(crabseal, hist=TRUE, trunc = 700)
-
